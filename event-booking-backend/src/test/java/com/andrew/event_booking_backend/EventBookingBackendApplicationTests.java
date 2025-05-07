c2e5d74a3922b0703c8dbcf0b560f60a18f2f477
Andrew Ezzat <andrewezzat21@gmail.com>
2025-05-07 22:13:47 +0300

    Initial Commit

---
+++ event-booking-backend/src/test/java/com/andrew/event_booking_backend/EventBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.event_booking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
